77ed56175705cbb48fd8f30e50fb0bc980493b63
olaplace <laplace.olivier@gmail.com>
2015-05-04 18:28:26 +0200

    Commit

---
+++ env.js
@@ -0,0 +1,24 @@
+var dev =  {
+  database: "Documents", // Each connection can specify its own database
+  host: "localhost",     // The host against which queries will be run
+  port: 8000,            // By default port 8000 accepts Client API requests
+  user: "admin",        // A user with at least the rest-writer role
+  password: "marklogic",  // Probably not your password
+  authType: "DIGEST"     // The default auth
+}
+
+// Another connection. Change the module.exports below to 
+// use it without having to change consuming code.
+/*
+var test =  {
+  database: "Documents",
+  host: "acceptance.example.com",
+  port: 9116,
+  user: "app-writer",
+  password: "********",
+  authType: "DIGEST"
+}
+*/
+module.exports = {
+  connection: dev       // Export the development connection
+}
