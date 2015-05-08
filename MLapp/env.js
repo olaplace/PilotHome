ed899b2051276bbce0ec0d49fae8ab239a5be1b0
olaplace <laplace.olivier@gmail.com>
2015-05-08 18:59:43 +0200

    Commit

--- MLapp/env.js
+++ MLapp/env.js
@@ -2,8 +2,8 @@ var dev =  {
   database: "Documents", // Each connection can specify its own database
   host: "localhost",     // The host against which queries will be run
   port: 8000,            // By default port 8000 accepts Client API requests
-  user: "admin",        // A user with at least the rest-writer role
-  password: "marklogic",  // Probably not your password
+  user: "admin",         // A user with at least the rest-writer role
+  password: "marklogic", // Probably not your password
   authType: "DIGEST"     // The default auth
 }
 
